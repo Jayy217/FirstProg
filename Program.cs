4cc2fa2273876f0120e43006e4743ec7a5516fc2
Jacob Youssif <103043338@student.swin.edu.au>
2020-02-25 16:30:17 +1100

    reads and prints full name

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace firstprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your first name?");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("What's your surname?");
+            string surname = Console.ReadLine();
+
+            string fullname = firstname + " " + surname; 
+
+            Console.WriteLine(fullname);
+        }
+    }
+}
